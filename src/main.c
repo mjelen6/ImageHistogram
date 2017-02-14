840877a478bf92f3545f5fc747ecea4146fe1d59
mjelen <mjelen@student.agh.edu.pl>
2017-02-15 00:59:49 +0100

    Baaardzo wstępne prace

--- src/main.c
+++ src/main.c
@@ -14,45 +14,50 @@
 #define hist_size 256
 
 int main(int argc, char* argv[]){
-	int  my_rank; /* rank of process */
-	int  p;       /* number of processes */
-	int source;   /* rank of sender */
-	int dest;     /* rank of receiver */
+
+	int my_rank;							// process rank
+	int ntasks;								// number of processes
+	int hist_table[hist_size] = { 0 };		// array for histogram
+
 	int tag=0;    /* tag for messages */
+
 	char message[100];        /* storage for message */
 	MPI_Status status ;   /* return status for receive */
 
 
-	start_time();
+
 
 
 	/* start up MPI */
 	MPI_Init(&argc, &argv);
-
 	/* find out process rank */
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-
+	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
 	/* find out number of processes */
-	MPI_Comm_size(MPI_COMM_WORLD, &p);
+	MPI_Comm_size(MPI_COMM_WORLD,&ntasks);
 
 
-	FreeImage_Initialise(TRUE);
-	FIBITMAP *bitmap = FreeImage_Load(FIF_JPEG, "../img/lenna512x512_gray.jpg",JPEG_DEFAULT);
-	if (bitmap){
-		unsigned width = FreeImage_GetWidth(bitmap);
-		unsigned height = FreeImage_GetHeight(bitmap);
-		printf("picture loaded width = %d, height = %d \n", width, height);
-	} else {
-		printf("picture not loaded \n ");
-	}
 
 
-	if (my_rank !=0){
+	if (my_rank ==0){
+
+
+
 		/* create message */
-		sprintf(message, "Hello MPI World from process %d!", my_rank);
-		dest = 0;
-		/* use strlen+1 so that '\0' get transmitted */
-		MPI_Send(message, strlen(message)+1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+		printf("Master process - start, id = %d", my_rank);
+
+		FreeImage_Initialise(TRUE);
+			FIBITMAP *bitmap = FreeImage_Load(FIF_JPEG, "../img/lenna512x512_gray.jpg",JPEG_DEFAULT);
+			if (bitmap){
+				unsigned width = FreeImage_GetWidth(bitmap);
+				unsigned height = FreeImage_GetHeight(bitmap);
+				printf("picture loaded width = %d, height = %d \n", width, height);
+				int bytespp = FreeImage_GetLine(bitmap) / width;
+				printf(" bytes per pixel = %d \n ", bytespp);
+			} else {
+				printf("picture not loaded \n ");
+			}
+
+
 	}
 	else{
 		printf("Hello MPI World From process 0: Num processes: %d\n",p);
@@ -61,13 +66,14 @@ int main(int argc, char* argv[]){
 			printf("%s\n",message);
 		}
 	}
+
+	MPI_Reduce( & iLiczba, & iWynik, 1, MPI_INT, MPI_SUM, 0 /*root*/, MPI_COMM_WORLD );
+	 printf( "%d, ", iWynik ); //Wszystkie procesy wypisują wynik
+
 	/* shut down MPI */
 	MPI_Finalize();
-
-	stop_time();
-	print_time("\nElapsed:");
-
 	FreeImage_DeInitialise();
+
 
 	return 0;
 }
