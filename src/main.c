666151957310ccf1489ce6f33d7eed508f01d873
mjelen <mjelen@student.agh.edu.pl>
2017-02-16 00:15:00 +0100

    Spięty freeimage z MPI. Problem z czytaniem cudzej pamięci

--- src/main.c
+++ src/main.c
@@ -1,80 +1,108 @@
-/*
- * main.c
- *
- *  Created on: 10.02.2017
- *      Author: maciek
- */
-
 #include <stdio.h>
 #include <string.h>
-#include <mpi.h>
+#include <stdlib.h>
+#include "mpi.h"
 #include <FreeImage.h>
 #include "my_timers.h"
 
-#define hist_size 256
+#define HIST_SIZE 256
 
-int main(int argc, char* argv[]){
-
-	int my_rank;							// process rank
-	int ntasks;								// number of processes
-	int hist_table[hist_size] = { 0 };		// array for histogram
-
-	int tag=0;    /* tag for messages */
-
-	char message[100];        /* storage for message */
-	MPI_Status status ;   /* return status for receive */
-
-
-
-
+int main(int argc, char* argv[]) {
+	int my_rank; /* rank of process */
+	int p; /* number of processes */
+	int source; /* rank of sender */
+	int dest; /* rank of receiver */
+	int tag = 0; /* tag for messages */
+	char message[100]; /* storage for message */
+	MPI_Status status; /* return status for receive */
+	int root = 0;
+	int bitmap_len, sub_bitmap_len;
+	char* sub_bitmap;
+	int i; /* uniwersal counter */
+	int check = 0; /**/
+	int hist_table[HIST_SIZE] = { 0 };
+	int sub_hist_table[HIST_SIZE] = { 0 };
 
 	/* start up MPI */
+
 	MPI_Init(&argc, &argv);
+
 	/* find out process rank */
-	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
 	/* find out number of processes */
-	MPI_Comm_size(MPI_COMM_WORLD,&ntasks);
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
+	//get image
+	FreeImage_Initialise(TRUE);
+	FIBITMAP *bitmap = FreeImage_Load(FIF_JPEG, "../img/lenna512x512_gray.jpg",
+			JPEG_DEFAULT);
+	if (!bitmap) {
+		printf("nie zaladowano obrazu");
+		return 0;
+	}
 
+	//image properties
+	int width = FreeImage_GetWidth(bitmap);
 
+	int height = FreeImage_GetHeight(bitmap);
+	int bytespp = FreeImage_GetLine(bitmap) / width;
+	bitmap_len = width * height;
+	printf("H%d %d %d", height, bytespp, bitmap_len);
 
-	if (my_rank ==0){
+	if (my_rank == root) {
 
+		printf("Hello MPI World From process 0: Num processes: %d\n", p);
+		for (source = 1; source < p; source++) {
+			MPI_Recv(message, 100, MPI_CHAR, source, tag,
+			MPI_COMM_WORLD, &status);
+			printf("%s\n", message);
+		}
 
-//dsv
+		printf("zaladowano obraz\n");
+		printf("picture loaded width = %d, height = %d \n", width, height);
+		printf(" bytes per pixel = %d \n ", bytespp);
+
+	} else {
 
 		/* create message */
-		printf("Master process - start, id = %d", my_rank);
-
-		FreeImage_Initialise(TRUE);
-			FIBITMAP *bitmap = FreeImage_Load(FIF_JPEG, "../img/lenna512x512_gray.jpg",JPEG_DEFAULT);
-			if (bitmap){
-				unsigned width = FreeImage_GetWidth(bitmap);
-				unsigned height = FreeImage_GetHeight(bitmap);
-				printf("picture loaded width = %d, height = %d \n", width, height);
-				int bytespp = FreeImage_GetLine(bitmap) / width;
-				printf(" bytes per pixel = %d \n ", bytespp);
-			} else {
-				printf("picture not loaded \n ");
-			}
-
+		sprintf(message, "Hello MPI World from process %d!", my_rank);
+		dest = 0;
+		/* use strlen+1 so that '\0' get transmitted */
+		MPI_Send(message, strlen(message) + 1, MPI_CHAR, dest, tag,
+				MPI_COMM_WORLD);
 
 	}
-	else{
-		printf("Hello MPI World From process 0: Num processes: %d\n",p);
-		for (source = 1; source < p; source++) {
-			MPI_Recv(message, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
-			printf("%s\n",message);
+
+	/* allocate sub_bitmap memory .
+	 * Scatter the initial data among the process*/
+	sub_bitmap_len = bitmap_len / p;
+	sub_bitmap = (char*)malloc(sub_bitmap_len);
+	MPI_Scatter(bitmap, sub_bitmap_len, MPI_CHAR, sub_bitmap, sub_bitmap_len,
+			MPI_CHAR, root, MPI_COMM_WORLD);
+
+	/*each process calc his own histogram on his amount of bitmaps*/
+	for (i = 0; i < sub_bitmap_len; i++) {
+		sub_hist_table[sub_bitmap[i]]++;
+	}
+
+	/*sum up all sub_histogram's to one */
+	MPI_Reduce(sub_hist_table, hist_table, HIST_SIZE, MPI_INT, MPI_SUM, root,
+			MPI_COMM_WORLD);
+
+	if (my_rank == root) {
+		for (i = 0; i < HIST_SIZE; i++) {
+			printf("%d ", hist_table[i]);
+			check += hist_table[i];
 		}
+		printf("\ncheck = %d, nummber of pixel = %d ", check, bitmap_len);
 	}
 
-	MPI_Reduce( & iLiczba, & iWynik, 1, MPI_INT, MPI_SUM, 0 /*root*/, MPI_COMM_WORLD );
-	 printf( "%d, ", iWynik ); //Wszystkie procesy wypisują wynik
+	free(sub_bitmap);
 
+	FreeImage_DeInitialise();
 	/* shut down MPI */
 	MPI_Finalize();
-	FreeImage_DeInitialise();
-
 
 	return 0;
 }
